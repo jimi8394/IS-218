8dcc0f5b657f1bc2fd8ca9cbd5a24d5151f73a2c
Jimmy <jimi8394@gmail.com>
2015-03-09 01:59:05 -0400

    Update Constructor.php

--- php/Constructor-class-HelloWorld-public-function-__construct-temp-print-Hello-temp/Constructor.php
+++ php/Constructor-class-HelloWorld-public-function-__construct-temp-print-Hello-temp/Constructor.php
@@ -1,10 +1,10 @@
 <?php
 // Constructor
 class HelloWorld {
-   public function __construct() {
-       print "Hello";
+   public function __construct($temp) {
+       print "Hello $temp";
    }
 }
 // In HelloWorld constructor
-$obj = new HelloWorld();
+$obj = new HelloWorld("World");
 ?>
